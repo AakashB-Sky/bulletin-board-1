9015879fd09b4e7c998ee2b3e114fe84d90ae687
AakashB-Sky <62520487+AakashB-Sky@users.noreply.github.com>
2025-02-28 22:35:14 +0000

    boards#index complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,4 @@
 Rails.application.routes.draw do
-  # Routes for the Post resource:
-
-  # CREATE
-  post("/insert_post", { :controller => "posts", :action => "create" })
-          
-  # READ
-  get("/posts", { :controller => "posts", :action => "index" })
-  
-  get("/posts/:path_id", { :controller => "posts", :action => "show" })
-  
-  # UPDATE
-  
-  post("/modify_post/:path_id", { :controller => "posts", :action => "update" })
-  
-  # DELETE
-  get("/delete_post/:path_id", { :controller => "posts", :action => "destroy" })
-
-  #------------------------------
-
   # Routes for the Board resource:
 
   # CREATE
@@ -34,6 +15,25 @@ Rails.application.routes.draw do
   
   # DELETE
   get("/delete_board/:path_id", { :controller => "boards", :action => "destroy" })
+
+  #------------------------------
+
+  # Routes for the Post resource:
+
+  # CREATE
+  post("/insert_post", { :controller => "posts", :action => "create" })
+          
+  # READ
+  get("/posts", { :controller => "posts", :action => "index" })
+  
+  get("/posts/:path_id", { :controller => "posts", :action => "show" })
+  
+  # UPDATE
+  
+  post("/modify_post/:path_id", { :controller => "posts", :action => "update" })
+  
+  # DELETE
+  get("/delete_post/:path_id", { :controller => "posts", :action => "destroy" })
 
   #------------------------------
 
